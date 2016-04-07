a39c7ea85bacf3eaadc65aa391bda189d585a424
Natasha Carlyon <natasha.carlyon@gmail.com>
2016-04-07 03:09:35 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+
+        max_palindrome = 0
+        (100..999).each do |i|
+                (100..999).each do |j|
+                product = i * j
+                if product.to_s == product.to_s.reverse && product > max_palindrome
+                        max_palindrome = product
+                end
+                end
+        end
+        max_palindrome
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+attr_reader :answer
+
+        def initialize
+                @answer = largest_palindrome_product
+        end
+
+        def largest_palindrome_product
+        max_palindrome = 0
+        (100..999).each do |i|
+                (100..999).each do |j|
+                product = i * j
+                if product.to_s == product.to_s.reverse && product > max_palindrome
+                        max_palindrome = product
+                end
+                end
+        end
+        max_palindrome
+end
+
+end
